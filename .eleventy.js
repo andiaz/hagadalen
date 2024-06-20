df9528442d27f8e4edf1b84f1700b8fa5ee26b77
andiaz <andreas.johansson.dev@gmail.com>
2024-06-20 22:56:01 +0200

    Started adding language support.

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,12 @@
+const { EleventyI18nPlugin } = require("@11ty/eleventy");
+
 module.exports = function (eleventyConfig) {
     
+    eleventyConfig.addPlugin(EleventyI18nPlugin, {
+		// any valid BCP 47-compatible language tag is supported
+		defaultLanguage: "sv",
+	});
+
     eleventyConfig.addPassthroughCopy("./src/css");
     eleventyConfig.addPassthroughCopy("./src/img");
     eleventyConfig.addPassthroughCopy("./src/js");
