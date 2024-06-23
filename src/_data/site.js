5abd83d9a8a8b62b63ca87a2a6b39a4cc16db451
andiaz <andreas.johansson.dev@gmail.com>
2024-06-23 17:57:49 +0200

    Added some general information about the website.

---
+++ src/_data/site.js
@@ -0,0 +1,19 @@
+module.exports = {
+    title: "Hågadalens Vandrarhem",
+    description:
+      "Hågadalens Hostel & Vandrarhem ligger i Uppsala, alldeles intill Hågadalen-Nåstens naturreservat. Här erbjuds bekväma, moderna rum med sittmöbler och trägolv. Uppsalas centralstation ligger 6 km bort och kan lätt nås med buss..",
+    url: "https://hagadalensvandrarhem.se/",
+    baseUrl: "/",
+    author: "Andreas Johansson",
+    buildTime: new Date(),
+    languages: [
+      {
+        label: "english",
+        code: "en",
+      },
+      {
+        label: "svenska",
+        code: "sv",
+      },
+    ],
+  };
